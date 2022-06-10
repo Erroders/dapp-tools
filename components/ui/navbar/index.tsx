d6bba5605a3aa711cae8fe59ddb30af392dd3e82
Sparsh Agarwal <sparshaga25@gmail.com>
2022-06-10 20:03:46 +0530

    Updated Navbar Address UI

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -10,12 +10,20 @@ const Navbar = ({ title = 'Dapp Tools', walletAddressText = 'Connect Wallet' }: 
         <header className="shadow-md sticky top-0 w-full bg-white z-50">
             <div className="max-w-screen-xl p-4 mx-auto">
                 <div className="flex items-center justify-between space-x-4 lg:space-x-10">
-                    <div className="flex lg:w-0 lg:flex-1 text-lg font-medium">{title}</div>
+                    <div className="flex lg:w-0 lg:flex-1 text-xl font-medium">{title}</div>
 
                     <div className="items-center justify-end flex space-x-4">
-                        <button className="px-5 py-2 text-sm font-medium text-gray-600 bg-gray-100 rounded-lg">
-                            {walletAddressText}
-                        </button>
+                        <div className="relative block group">
+                            {/* <button className="px-5 py-2 text-sm font-medium text-gray-600 bg-gray-100 rounded-lg">
+                                <span className="absolute inset-0 border-2 border-black border-dashed"></span>
+                                {walletAddressText}
+                            </button> */}
+
+                            <span className="absolute inset-0 border-2 border-black border-dashed"></span>
+                            <button className="relative p-2 text-xs flex items-end h-full transition-transform transform bg-white border-2 border-black group-hover:-translate-x-2 group-hover:-translate-y-2">
+                                {walletAddressText}
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
