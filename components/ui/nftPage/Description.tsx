0c090590052ee83bc05d47814b542739486cecf7
Sparsh Agarwal <sparshaga25@gmail.com>
2022-06-13 20:18:16 +0530

    Changed svg path tags to camelCase

--- components/ui/nftPage/Description.tsx
+++ components/ui/nftPage/Description.tsx
@@ -15,7 +15,7 @@ const Description = ({ description }: { description: string }) => {
                             viewBox="0 0 24 24"
                             stroke="currentColor"
                         >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
                         </svg>
                     </span>
                 </summary>
--- components/ui/nftPage/Properties.tsx
+++ components/ui/nftPage/Properties.tsx
@@ -21,7 +21,7 @@ const Properties = ({ data }: { data: Array<PropertiesProps> }) => {
                             viewBox="0 0 24 24"
                             stroke="currentColor"
                         >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
                         </svg>
                     </span>
                 </summary>
