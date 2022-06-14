0f3424f6bbd44fb23024c1b1f044521298d87098
Sparsh Agarwal <sparshaga25@gmail.com>
2022-06-14 18:13:16 +0530

    Added Dropdown Menu

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import Link from 'next/link';
+import React, { useContext, useEffect, useState } from 'react';
 import { WalletContext } from '../../../pages/_app';
 
 interface NavbarProps {
@@ -6,8 +7,24 @@ interface NavbarProps {
     walletAddressText?: string;
 }
 
-const Navbar = ({ title = 'Dapp Tools', walletAddressText = 'Connect Wallet' }: NavbarProps) => {
+const Navbar = ({ title = 'Dapp Tools' }: NavbarProps) => {
     const walletContext = useContext(WalletContext);
+    const [networkName, setNetworkName] = useState('Not Connected');
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    useEffect(() => {
+        walletContext.web3Provider?.getNetwork().then((v) => {
+            setNetworkName(v.name);
+        });
+    }, []);
+
+    const handleWalletBtnClick = () => {
+        if (walletContext.web3Provider) {
+            setDropdownOpen(!dropdownOpen);
+        } else {
+            walletContext.updateConnectWalletModalVisibility(true);
+        }
+    };
 
     return (
         <header className="shadow-md sticky top-0 w-full bg-white z-30">
@@ -17,37 +34,42 @@ const Navbar = ({ title = 'Dapp Tools', walletAddressText = 'Connect Wallet' }: 
 
                     <div className="items-center justify-end flex space-x-4">
                         <div className="relative block group">
-                            {/* <button className="px-5 py-2 text-sm font-medium text-gray-600 bg-gray-100 rounded-lg">
-                                <span className="absolute inset-0 border-2 border-black border-dashed"></span>
-                                {walletAddressText}
-                            </button> */}
-
                             <span className="absolute inset-0 border-2 border-black border-dashed"></span>
-                            <button
-                                className="relative p-2 text-xs flex items-end h-full transition-transform transform bg-white border-2 border-black group-hover:-translate-x-2 group-hover:-translate-y-2"
-                                // onClick={() => {
-                                //     walletContext.updateConnectWalletModalVisibility(true);
-                                // }}
-                            >
-                                {walletContext.chainId || 'Not Connected'}
+                            <button className="relative p-2 text-xs cursor-default flex items-end h-full transition-transform transform bg-white border-2 border-black group-hover:-translate-x-1 group-hover:-translate-y-1">
+                                {networkName || 'Not Connected'}
                             </button>
                         </div>
 
                         <div className="relative block group">
-                            {/* <button className="px-5 py-2 text-sm font-medium text-gray-600 bg-gray-100 rounded-lg">
-                                <span className="absolute inset-0 border-2 border-black border-dashed"></span>
-                                {walletAddressText}
-                            </button> */}
-
                             <span className="absolute inset-0 border-2 border-black border-dashed"></span>
                             <button
-                                className="relative p-2 text-xs flex items-end h-full transition-transform transform bg-white border-2 border-black group-hover:-translate-x-2 group-hover:-translate-y-2"
-                                onClick={() => {
-                                    walletContext.updateConnectWalletModalVisibility(true);
-                                }}
+                                className="relative p-2 text-xs flex items-end h-full transition-transform transform bg-white border-2 border-black group-hover:-translate-x-1.5 group-hover:-translate-y-1.5"
+                                onClick={handleWalletBtnClick}
                             >
                                 {walletContext.walletAddress || 'Connect Wallet'}
                             </button>
+
+                            {dropdownOpen && (
+                                <div className="h-auto w-full absolute bg-white mt-1.5">
+                                    <span className="absolute inset-0 border-2 border-black border-dashed"></span>
+                                    <ul className="text-right px-3 py-1.5 bg-white border-2 border-black group-hover:-translate-x-1 group-hover:-translate-y-1">
+                                        <li className="hover:font-semibold cursor-pointer">
+                                            <Link href={'/profile'}>Profile</Link>
+                                        </li>
+                                        <li
+                                            className="hover:font-semibold cursor-pointer"
+                                            onClick={() => {
+                                                walletContext.updateWalletAddress('');
+                                                walletContext.updateChainid(0);
+                                                walletContext.updateWeb3Provider(null);
+                                                setDropdownOpen(false);
+                                            }}
+                                        >
+                                            Disconnect
+                                        </li>
+                                    </ul>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
