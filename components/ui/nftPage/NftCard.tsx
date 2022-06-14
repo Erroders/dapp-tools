23bc2b4d0358d11b5147b5d874cfa6cd1866c365
Sparsh Agarwal <sparshaga25@gmail.com>
2022-06-14 12:41:17 +0530

    Removed NftCard TODO

--- components/ui/nftPage/NftCard.tsx
+++ components/ui/nftPage/NftCard.tsx
@@ -25,10 +25,7 @@ const NftCard = ({ token_id, contract_address, contract_name, logo_url, chainId 
 
             <div className="relative flex items-end h-full transition-transform transform bg-white border-2 border-black hover:-translate-x-2 hover:-translate-y-2">
                 <div className="transition-opacity ">
-                    {/* TODO: covalent logo url :angry_face */}
-                    {/* <img src={logo_url} alt="" /> */}
-
-                    <img src={imageUrl} />
+                    <img src={imageUrl} alt={contract_name + ' #' + token_id} />
 
                     <h4 className="px-2 py-2">
                         <span className="text-sm font-medium">{contract_name}</span>
