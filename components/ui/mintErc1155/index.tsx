7b7481c003a380fd758eb019d03f2645bf5fe372
Sparsh Agarwal <sparshaga25@gmail.com>
2022-06-17 00:04:41 +0530

    Completed ERC1155 Contract

--- components/ui/mintErc1155/index.tsx
+++ components/ui/mintErc1155/index.tsx
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { WalletContext } from '../../../pages/_app';
 import uploadIpfsData from '../../../utils/nft/uploadIpfsData';
 import { Button, CheckboxInput, ImageInput, TextInput, TextInputTypes } from '../generalComponents';
-import RadioInput from '../generalComponents/RadioInput';
+import DropdownInput from '../generalComponents/DropdownInput';
+import networksData from '../../../data/networks.json';
+import { ERC1155Data } from '../../../pages/api/erc1155';
+import { deployContract } from '../../../utils/contract_deployer';
+import { ERCs } from '../../../utils/types';
+import path from 'path';
 
 const MintErc1155 = () => {
     const [name, setName] = useState('');
-    // const [uriStorage, setUriStorage] = useState(true);
     const [burnable, setBurnable] = useState(false);
     const [pausable, setPausable] = useState(false);
-    const [mintable, setMintable] = useState(true);
+    const [mintable, setMintable] = useState(false);
     const [supply, setSupply] = useState(false);
     const [access, setAccess] = useState('Ownable');
+    const [upgradeable, setUpgradeable] = useState('false');
     const [securityContract, setSecurityContract] = useState('');
     const [license, setLicense] = useState('');
+    const [networkName, setNetworkName] = useState('');
 
     const [nftImage, setNftImage] = useState<File>();
     const [nftName, setNftName] = useState('');
     const [nftDescription, setNftDescription] = useState('');
     const [nftExternalUrl, setNftExternalUrl] = useState('');
+    const [nftQuantity, setNftQuantity] = useState('1');
+
+    const [afterDeploymentDesc, setAfterDeploymentDesc] = useState('');
 
     const [step1Open, setStep1Open] = useState(true);
     const [step2Open, setStep2Open] = useState(false);
+    const [step3Open, setStep3Open] = useState(false);
+
+    const walletContext = useContext(WalletContext);
+    walletContext.web3Provider?.getNetwork().then((v) => {
+        setNetworkName(v.name);
+    });
 
     const handleImageChange = (imageFile: File) => {
         setNftImage(imageFile);
@@ -29,19 +45,50 @@ const MintErc1155 = () => {
     const handleStep1Submit = () => {
         console.log('Clicked Next');
 
-        if (!name || !access || !securityContract || !license) {
+        if (!name || !access || !upgradeable || !securityContract || !license) {
             return;
         }
 
         setStep1Open(false);
         setStep2Open(true);
     };
+
     const handleStep2Submit = async () => {
         console.log('Clicked Mint');
 
-        if (!nftImage || !nftName || !nftDescription || !nftExternalUrl) {
+        if (!nftImage || !nftName || !nftDescription || !nftQuantity) {
             return;
         }
+
+        if (!(access == 'ownable' || access == 'roles' || access == undefined)) {
+            return;
+        }
+
+        const nets = Object.values(networksData).map((n) => {
+            return n.network;
+        });
+
+        const currentNetwork = await walletContext.web3Provider?.getNetwork();
+        if (!currentNetwork) {
+            return;
+        }
+
+        const currentChainId = currentNetwork.chainId.toString();
+        const currentNetworkName = currentNetwork.name;
+
+        if (!nets.includes(currentNetworkName)) {
+            return;
+        }
+
+        // TODO: ERROR: Temporary solution for deployContract
+        if (!(currentChainId == '80001' || currentChainId == '137')) {
+            return;
+        }
+
+        setStep2Open(false);
+        setStep3Open(true);
+
+        setAfterDeploymentDesc(afterDeploymentDesc + 'Uploading NFT data . . .\n');
 
         const metadata = await uploadIpfsData({
             name: nftName,
@@ -50,12 +97,54 @@ const MintErc1155 = () => {
             image: nftImage,
         });
 
-        console.log(metadata);
+        // console.log(metadata);
 
-        // TODO: Upload Image on NFT Storage
-        // TODO: Upload Data on NFT Storage
-        // TODO: Generate Contract
-        // TODO: Mint NFT
+        const erc1155pts: ERC1155Data = {
+            name: name,
+            uri: metadata.url,
+            burnable: burnable,
+            pausable: pausable,
+            mintable: mintable,
+            supply: supply,
+            accesss: access,
+            info: {
+                securityContact: securityContract,
+                license: license,
+            },
+        };
+
+        setAfterDeploymentDesc(afterDeploymentDesc + 'Generating Contarct . . .\n');
+
+        const contractDetailsPromise = deployContract(
+            erc1155pts,
+            ERCs.ERC1155,
+            walletContext.web3Provider,
+            currentChainId,
+        );
+
+        setAfterDeploymentDesc(afterDeploymentDesc + 'Deploying Contarct . . .\nAwaiting Wallet Confirmation . . .\n');
+
+        const contractDetails = await contractDetailsPromise;
+
+        // console.log(contractDetails);
+
+        if (contractDetails) {
+            setAfterDeploymentDesc(afterDeploymentDesc + 'Deplyoment Successful . . .\n');
+            setAfterDeploymentDesc(afterDeploymentDesc + '\n\n');
+            setAfterDeploymentDesc(
+                afterDeploymentDesc + 'Contarct Address: ' + contractDetails.contractAddress + ' \n',
+            );
+
+            const conrtractExplorerUrl =
+                new URL(
+                    path.join('tx', contractDetails.contractAddress.split('tx/')[1]),
+                    networksData[currentChainId].blockExplorerURL,
+                ).toString() + '/';
+
+            setAfterDeploymentDesc(afterDeploymentDesc + 'View Details on: ' + conrtractExplorerUrl + ' \n');
+        } else {
+            setAfterDeploymentDesc(afterDeploymentDesc + 'Deplyoment Failed . . .\n');
+        }
     };
 
     return (
@@ -64,8 +153,17 @@ const MintErc1155 = () => {
                 <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
                     <div className="sm:justify-between sm:items-center sm:flex">
                         <div className="text-center sm:text-left">
-                            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">NFT Collection</h1>
-                            <p className="mt-1.5 text-sm tracking-wide text-gray-500">Mint multiple ERC-1155 NFTs</p>
+                            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">ERC1155</h1>
+                            <p className="mt-1.5 text-sm tracking-wide text-gray-500">
+                                A standard interface for contracts that manage multiple token types. A single deployed
+                                contract may include any combination of fungible tokens, non-fungible tokens or other
+                                configurations (e.g. semi-fungible tokens). The idea is simple and seeks to create a
+                                smart contract interface that can represent and control any number of fungible and
+                                non-fungible token types. In this way, the ERC-1155 token can do the same functions as
+                                an ERC-20 and ERC-721 token, and even both at the same time. And best of all, improving
+                                the functionality of both standards, making it more efficient, and correcting obvious
+                                implementation errors on the ERC-20 and ERC-721 standards.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -80,8 +178,8 @@ const MintErc1155 = () => {
                         }}
                     >
                         <div>
-                            <h2 className="text-xl font-semibold">Token Details</h2>
-                            <p className="text-sm ml-0.5">Enter token details and choose your network</p>
+                            <h2 className="text-xl font-semibold">Contract Details</h2>
+                            <p className="text-sm ml-0.5">Enter Contract Details and choose your Network</p>
                         </div>
                     </summary>
 
@@ -102,8 +200,10 @@ const MintErc1155 = () => {
                                 <CheckboxInput id="pausable" label="Pausable" value={pausable} setValue={setPausable} />
                                 <CheckboxInput id="mintable" label="Mintable" value={mintable} setValue={setMintable} />
                                 <CheckboxInput id="supply" label="Supply" value={supply} setValue={setSupply} />
+                            </div>
 
-                                <RadioInput
+                            <div className="grid grid-cols-2 gap-4">
+                                <DropdownInput
                                     id="accesss"
                                     label="Access Control"
                                     value={access}
@@ -118,6 +218,27 @@ const MintErc1155 = () => {
                                             label: 'Roles',
                                         },
                                     ]}
+                                />
+                                <DropdownInput
+                                    id="upgradeable"
+                                    label="Upgradeable"
+                                    value={upgradeable}
+                                    setValue={setUpgradeable}
+                                    valueOptions={[
+                                        {
+                                            value: 'false',
+                                            label: 'False',
+                                        },
+                                        {
+                                            value: 'transparent',
+                                            label: 'Transparent',
+                                        },
+                                        {
+                                            value: 'uups',
+                                            label: 'Uups',
+                                        },
+                                    ]}
+                                    disabled
                                 />
                             </div>
 
@@ -136,8 +257,17 @@ const MintErc1155 = () => {
                                 setValue={setLicense}
                             />
 
+                            <TextInput
+                                id="network"
+                                label="Network"
+                                type={TextInputTypes.TEXT}
+                                value={networkName}
+                                setValue={setNetworkName}
+                                disabled={true}
+                            />
+
                             <Button
-                                title="Next"
+                                title="Deploy"
                                 onClick={() => {
                                     handleStep1Submit();
                                 }}
@@ -155,8 +285,8 @@ const MintErc1155 = () => {
                         }}
                     >
                         <div>
-                            <h2 className="text-xl font-semibold">NFT Details</h2>
-                            <p className="text-sm ml-0.5">Enter NFT Details and upload Image</p>
+                            <h2 className="text-xl font-semibold">Token Details</h2>
+                            <p className="text-sm ml-0.5">Upload image and enter Token Details</p>
                         </div>
                     </summary>
 
@@ -196,6 +326,15 @@ const MintErc1155 = () => {
                                 setValue={setNftExternalUrl}
                             />
 
+                            <TextInput
+                                id="nftQuantity"
+                                label="nftQuantity"
+                                type={TextInputTypes.NUMBER}
+                                value={nftQuantity}
+                                setValue={setNftQuantity}
+                                minNum={1}
+                            />
+
                             <Button
                                 title="Mint"
                                 onClick={() => {
@@ -206,23 +345,27 @@ const MintErc1155 = () => {
                         </div>
                     </div>
                 </details>
+
+                <details id="step3" className="bg-white border border-black divide-gray-200 p-6" open={step3Open}>
+                    <summary
+                        className="flex cursor-pointer"
+                        onClick={(e) => {
+                            e.preventDefault();
+                        }}
+                    >
+                        <div>
+                            <h2 className="text-xl font-semibold">Deployment Details</h2>
+                            <p className="text-sm ml-0.5">Find Contarct Deployment details</p>
+                        </div>
+                    </summary>
+
+                    <hr className="my-3 border-gray-300" />
+
+                    <p className="whitespace-pre-line">{afterDeploymentDesc}</p>
+                </details>
             </div>
         </div>
     );
 };
 
 export default MintErc1155;
-
-// {
-//     "name": "TestNFTCollection",
-//     "uri": "dhsgfdshgvjaghvdf",
-//     "burnable": false,
-//     "pausable": false,
-//     "mintable": false,
-//     "supply": true,
-//     "accesss": "ownable",
-//     "info": {
-//         "securityContact": "rg@email.com",
-//         "license": "MIT"
-//     }
-// }
